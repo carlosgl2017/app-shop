10209e76315a6efc57087fdf7e80c996272d42a0
Juan Carlos Garcia Lima <carlos2017gl@gmail.com>
2021-04-29 09:34:52 -0400

    crud de ventas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/views/home/home.component';
+import { VentaCreateComponent } from './components/views/venta/venta-create/venta-create.component';
+import { VentaDeleteComponent } from './components/views/venta/venta-delete/venta-delete.component';
+import { VentaReadComponent } from './components/views/venta/venta-read/venta-read.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'ventas',
+    component:VentaReadComponent
+  },
+  {
+    path:'ventas/create',
+    component:VentaCreateComponent
+  },
+  {
+    path:'ventas/delete/:venid',
+    component:VentaDeleteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from "@angular/forms";
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { VentaReadComponent } from './components/views/venta/venta-read/venta-read.component';
+import { VentaCreateComponent } from './components/views/venta/venta-create/venta-create.component';
+import { VentaDeleteComponent } from './components/views/venta/venta-delete/venta-delete.component';
+import { VentaUpdateComponent } from './componets/views/venta/venta-update/venta-update.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    VentaReadComponent,
+    VentaCreateComponent,
+    VentaDeleteComponent,
+    VentaUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    HttpClientModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule
+    
+
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
